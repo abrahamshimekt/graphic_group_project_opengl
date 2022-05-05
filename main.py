f0be42d9414d4baa45dc545031ff939062387eb7
Abraham <abrahamshimekt4096@gmail.com>
2022-05-05 22:35:50 +0300

    organize each 2d graphics under a function

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import math
-from tkinter import *  # from tkinter module import all classes
 import pygame  # import a pygame class
 from OpenGL.GL import *  # from python opengl.gl module import all classes
 from OpenGL.GLU import *  # from opengl.glu module import all classes
-from OpenGL.raw.GLUT import glutSolidSphere, glutSolidCube, glutSwapBuffers
 from pygame.locals import *  # from pygame.locals import all class
 
 """ Init function initialize the pygame display window with width, height and colors
@@ -18,16 +16,9 @@ def init():
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     gluOrtho2D(0, 100, 0, 100)
-def draw_house():
-    """a"""
-    glClear(GL_COLOR_BUFFER_BIT)
-    glColor3f(0.0, 0.8, 0.2)
-    glBegin(GL_POLYGON)
-    glVertex2d(0, 0)
-    glVertex2d(100, 0)
-    glVertex2d(100, 60)
-    glVertex2d(0, 60)
-    glEnd()
+
+
+def draw_sky():
     """sky"""
     glColor3f(0.0, 1.0, 1.0)
     glBegin(GL_POLYGON)
@@ -36,41 +27,21 @@ def draw_house():
     glVertex2d(100, 100)
     glVertex2d(0, 100)
     glEnd()
-    """mountain"""
-    glColor3f(0.1, 0.7, 0.1)
-    glBegin(GL_POLYGON)
-    glVertex2d(0, 50)
-    glVertex2d(0, 70)
-    glVertex2d(100, 80)
-    glVertex2d(100, 70)
-    glVertex2d(55, 70)
-    glVertex2d(0, 85)
-    glEnd()
+
+
+def draw_sun():
     """sun"""
-    glColor3f(0.9601,0.956,0.502)
+    glColor3f(0.9601, 0.956, 0.502)
     glBegin(GL_POLYGON)
     for i in range(361):
-        angle = 2*math.pi*i/360
-        x = 20 + 3*math.cos(angle)
-        y = 85+ 3*math.sin(angle)
-        glVertex2d(x,y)
+        angle = 2 * math.pi * i / 360
+        x = 20 + 3 * math.cos(angle)
+        y = 85 + 3 * math.sin(angle)
+        glVertex2d(x, y)
     glEnd()
-    """field"""
-    glColor3f(0.0, 0.8, 0.2)
-    glBegin(GL_POLYGON)
-    glVertex2d(0, 70)
-    glVertex2d(100, 70)
-    glVertex2d(100, 40)
-    glVertex2d(0, 40)
-    glEnd()
-    """road"""
-    glColor3f(0.4, 0.0, 0.0)
-    glBegin(GL_POLYGON)
-    glVertex2d(0, 5)
-    glVertex2d(100, 5)
-    glVertex2d(100, 10)
-    glVertex2d(0, 10)
-    glEnd()
+
+
+def draw_fence():
     """fence"""
     glColor3f(0.0, 0.0, 0.0)
     glBegin(GL_POLYGON)
@@ -85,13 +56,12 @@ def draw_house():
     glBegin(GL_POLYGON)
     a = 0
     for i in range(21):
-
         x = 2
-        a +=x
-        glVertex2d(0 +a, 17)
-        glVertex2d(2 +a, 17)
-        glVertex2d(2 +a, 10)
-        glVertex2d(0 +a, 10)
+        a += x
+        glVertex2d(0 + a, 17)
+        glVertex2d(2 + a, 17)
+        glVertex2d(2 + a, 10)
+        glVertex2d(0 + a, 10)
 
     glEnd()
     """fence"""
@@ -107,6 +77,8 @@ def draw_house():
 
     glEnd()
 
+
+def draw_bushes():
     """bush"""
     glColor3f(0.0, 0.6, 0.0)
     glBegin(GL_TRIANGLES)
@@ -128,7 +100,10 @@ def draw_house():
     glVertex2d(49, 60)
     glVertex2d(46, 62)
     glEnd()
-    """flag"""
+
+
+def draw_flag():
+    """green"""
     glColor3f(0.04, .93, 0.04)
     glBegin(GL_POLYGON)
     glVertex2d(15, 55)
@@ -136,6 +111,7 @@ def draw_house():
     glVertex2d(25, 53)
     glVertex2d(15, 53)
     glEnd()
+    """yellow"""
     glColor3f(1.0, 1.0, 0.0)
     glBegin(GL_POLYGON)
     glVertex2d(15, 53)
@@ -143,6 +119,7 @@ def draw_house():
     glVertex2d(25, 51)
     glVertex2d(15, 51)
     glEnd()
+    """red"""
     glColor3f(1.0, 0.0, 0.0)
     glBegin(GL_POLYGON)
     glVertex2d(15, 51)
@@ -150,6 +127,7 @@ def draw_house():
     glVertex2d(25, 49)
     glVertex2d(15, 49)
     glEnd()
+    """bar"""
     glColor3f(0.0, 0.0, 0.0)
     glBegin(GL_POLYGON)
     glVertex2d(15, 49)
@@ -158,6 +136,158 @@ def draw_house():
     glVertex2d(15, 30)
     glEnd()
 
+
+def draw_tree():
+    """root """
+    glColor3f(0.5, 0.0, 0.1)
+    glBegin(GL_TRIANGLES)
+    glVertex2d(5, 50)
+    glVertex2d(11, 50)
+    glVertex2d(8, 55)
+    glEnd()
+
+    """tree"""
+    glColor3f(0.0, 0.6, 0.0)
+    glBegin(GL_POLYGON)
+    glVertex2d(9, 63)
+    glVertex2d(12, 60)
+    glVertex2d(12, 64)
+    glVertex2d(15, 67)
+    glVertex2d(14, 70)
+    glVertex2d(15, 73)
+    glVertex2d(11, 77)
+    glVertex2d(9, 81)
+    glVertex2d(6, 77)
+    glVertex2d(3, 75)
+    glVertex2d(1, 72)
+    glVertex2d(2, 66)
+    glVertex2d(4, 65)
+    glVertex2d(4, 61)
+    glVertex2d(5, 63)
+    glVertex2d(7, 63)
+    glVertex2d(9, 63)
+    glEnd()
+    """trunck"""
+    glColor3f(0.5, 0.0, 0.1)
+    glBegin(GL_POLYGON)
+    glVertex2d(7, 53)
+    glVertex2d(9, 53)
+    glVertex2d(9, 66)
+    glVertex2d(7, 66)
+    glEnd()
+    """branch"""
+    glColor3f(0.5, 0.0, 0.1)
+    glBegin(GL_POLYGON)
+    glVertex2d(9, 65)
+    glVertex2d(12, 68)
+    glVertex2d(10, 70)
+    glVertex2d(8, 66)
+    glVertex2d(6, 72)
+    glVertex2d(5, 70)
+    glVertex2d(7, 65)
+    glEnd()
+
+    """root """
+    glColor3f(0.5, 0.0, 0.1)
+    glBegin(GL_TRIANGLES)
+    glVertex2d(88, 50)
+    glVertex2d(94, 50)
+    glVertex2d(91, 55)
+    glEnd()
+
+    """tree"""
+    glColor3f(0.0, 0.6, 0.0)
+    glBegin(GL_POLYGON)
+    glVertex2d(92, 63)
+    glVertex2d(95, 60)
+    glVertex2d(95, 64)
+    glVertex2d(98, 67)
+    glVertex2d(97, 70)
+    glVertex2d(98, 73)
+    glVertex2d(94, 77)
+    glVertex2d(92, 81)
+    glVertex2d(89, 77)
+    glVertex2d(86, 75)
+    glVertex2d(84, 72)
+    glVertex2d(85, 66)
+    glVertex2d(87, 65)
+    glVertex2d(87, 61)
+    glVertex2d(88, 63)
+    glVertex2d(90, 63)
+    glVertex2d(92, 63)
+    glEnd()
+    """trunck"""
+    glColor3f(0.5, 0.0, 0.1)
+    glBegin(GL_POLYGON)
+    glVertex2d(90, 53)
+    glVertex2d(92, 53)
+    glVertex2d(92, 66)
+    glVertex2d(90, 66)
+    glEnd()
+    """branch"""
+    glColor3f(0.5, 0.0, 0.1)
+    glBegin(GL_POLYGON)
+    glVertex2d(92, 65)
+    glVertex2d(95, 68)
+    glVertex2d(93, 70)
+    glVertex2d(91, 66)
+    glVertex2d(89, 72)
+    glVertex2d(88, 70)
+    glVertex2d(90, 65)
+    glEnd()
+
+
+def draw_road():
+    """road"""
+    glColor3f(0.4, 0.0, 0.0)
+    glBegin(GL_POLYGON)
+    glVertex2d(0, 5)
+    glVertex2d(100, 5)
+    glVertex2d(100, 10)
+    glVertex2d(0, 10)
+    glEnd()
+    draw_fence()
+    draw_bushes()
+    draw_flag()
+
+
+def draw_field():
+    """field"""
+    glColor3f(0.0, 0.8, 0.2)
+    glBegin(GL_POLYGON)
+    glVertex2d(0, 70)
+    glVertex2d(100, 70)
+    glVertex2d(100, 40)
+    glVertex2d(0, 40)
+    glEnd()
+
+
+def draw_mountain():
+    """mountain"""
+    glColor3f(0.1, 0.7, 0.1)
+    glBegin(GL_POLYGON)
+    glVertex2d(0, 50)
+    glVertex2d(0, 70)
+    glVertex2d(100, 80)
+    glVertex2d(100, 70)
+    glVertex2d(55, 70)
+    glVertex2d(0, 85)
+    glEnd()
+
+
+def draw_beside():
+    """a"""
+    glClear(GL_COLOR_BUFFER_BIT)
+    glColor3f(0.0, 0.8, 0.2)
+    glBegin(GL_POLYGON)
+    glVertex2d(0, 0)
+    glVertex2d(100, 0)
+    glVertex2d(100, 60)
+    glVertex2d(0, 60)
+    glEnd()
+
+
+def draw_house():
     """h1r1"""
     glColor3f(0.8, 0.8, 0.8)
     glBegin(GL_POLYGON)
@@ -249,7 +379,6 @@ def draw_house():
     glVertex2d(25, 65)
     glVertex2d(30, 70)
     glEnd()
-
     """h3r1"""
     glColor3f(0.8, 0.8, 0.8)
     glBegin(GL_POLYGON)
@@ -474,103 +603,19 @@ def draw_house():
     glVertex2d(48, 80)
     glEnd()
 
-    """root """
-    glColor3f(0.5, 0.0, 0.1)
-    glBegin(GL_TRIANGLES)
-    glVertex2d(5, 50)
-    glVertex2d(11, 50)
-    glVertex2d(8, 55)
-    glEnd()
 
-    """tree"""
-    glColor3f(0.0, 0.6, 0.0)
-    glBegin(GL_POLYGON)
-    glVertex2d(9, 63)
-    glVertex2d(12, 60)
-    glVertex2d(12, 64)
-    glVertex2d(15, 67)
-    glVertex2d(14, 70)
-    glVertex2d(15, 73)
-    glVertex2d(11, 77)
-    glVertex2d(9, 81)
-    glVertex2d(6, 77)
-    glVertex2d(3, 75)
-    glVertex2d(1, 72)
-    glVertex2d(2, 66)
-    glVertex2d(4, 65)
-    glVertex2d(4, 61)
-    glVertex2d(5, 63)
-    glVertex2d(7, 63)
-    glVertex2d(9, 63)
-    glEnd()
-    """trunck"""
-    glColor3f(0.5, 0.0, 0.1)
-    glBegin(GL_POLYGON)
-    glVertex2d(7, 53)
-    glVertex2d(9, 53)
-    glVertex2d(9, 66)
-    glVertex2d(7, 66)
-    glEnd()
-    """branch"""
-    glColor3f(0.5, 0.0, 0.1)
-    glBegin(GL_POLYGON)
-    glVertex2d(9, 65)
-    glVertex2d(12, 68)
-    glVertex2d(10, 70)
-    glVertex2d(8, 66)
-    glVertex2d(6, 72)
-    glVertex2d(5, 70)
-    glVertex2d(7, 65)
-    glEnd()
-
-    """root """
-    glColor3f(0.5, 0.0, 0.1)
-    glBegin(GL_TRIANGLES)
-    glVertex2d(88, 50)
-    glVertex2d(94, 50)
-    glVertex2d(91, 55)
-    glEnd()
-
-    """tree"""
-    glColor3f(0.0, 0.6, 0.0)
-    glBegin(GL_POLYGON)
-    glVertex2d(92, 63)
-    glVertex2d(95, 60)
-    glVertex2d(95, 64)
-    glVertex2d(98, 67)
-    glVertex2d(97, 70)
-    glVertex2d(98, 73)
-    glVertex2d(94, 77)
-    glVertex2d(92, 81)
-    glVertex2d(89, 77)
-    glVertex2d(86, 75)
-    glVertex2d(84, 72)
-    glVertex2d(85, 66)
-    glVertex2d(87, 65)
-    glVertex2d(87, 61)
-    glVertex2d(88, 63)
-    glVertex2d(90, 63)
-    glVertex2d(92, 63)
-    glEnd()
-    """trunck"""
-    glColor3f(0.5, 0.0, 0.1)
-    glBegin(GL_POLYGON)
-    glVertex2d(90, 53)
-    glVertex2d(92, 53)
-    glVertex2d(92, 66)
-    glVertex2d(90, 66)
-    glEnd()
-    """branch"""
-    glColor3f(0.5, 0.0, 0.1)
-    glBegin(GL_POLYGON)
-    glVertex2d(92, 65)
-    glVertex2d(95, 68)
-    glVertex2d(93, 70)
-    glVertex2d(91, 66)
-    glVertex2d(89, 72)
-    glVertex2d(88, 70)
-    glVertex2d(90, 65)
-    glEnd()
+def draw():
+    draw_beside()
+    draw_mountain()
+    draw_field()
+    draw_road()
+    draw_sky()
+    draw_sun()
+    draw_flag()
+    draw_fence()
+    draw_house()
+    draw_bushes()
+    draw_tree()
     glFlush()
 
 
@@ -581,7 +626,7 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            draw_house()
+            draw()
         pygame.display.flip()
         pygame.time.wait(10)
 
