2863730336429e4039fb5d2e593d26de75666960
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-06-19 00:24:29 +0300

    texture loader updated

--- primary_school/model_texture_loader.py
+++ primary_school/model_texture_loader.py
@@ -4,18 +4,19 @@ from OpenGL.GL import glBindTexture, glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_
 import pygame
 
 
-def load_texture(path, texture):
-    glBindTexture(GL_TEXTURE_2D, texture)
-    # Set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-    # Set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-    # load image
-    image = pygame.image.load(path)
-    image = pygame.transform.flip(image, False, True)
-    image_width, image_height = image.get_rect().size
-    img_data = pygame.image.tostring(image, "RGBA")
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
-    return texture
+class Texture_loader:
+    def __int__(self, texture_name):
+        self.texture_name = texture_name
+
+    def load_texture(self, path, texture):
+        glBindTexture(GL_TEXTURE_2D, texture)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        image = pygame.image.load(path)
+        image = pygame.transform.flip(image, False, True)
+        image_width, image_height = image.get_rect().size
+        img_data = pygame.image.tostring(image, "RGBA")
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+        return texture
